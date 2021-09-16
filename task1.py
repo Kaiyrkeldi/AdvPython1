a24ff7807fe5ec2e9c95a669f192f9301b778d19
Kaiyrkeldi <67787196+Kaiyrkeldi@users.noreply.github.com>
2021-09-16 12:47:05 +0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 #Kabken Kaiyrkeldi SE-2008
-from itertools import permutations
+# from itertools import permutations
 
 def func():
     list_a = []
@@ -8,5 +8,15 @@ def func():
     for i in range(0, a):
         list_item = input()
         list_a.append(list_item)
-    print(list(permutations(list_a)))
-func()
+    print(permute(list_a))
+def permute(s):
+    ch = list(s)
+    if len(ch) == 2:
+        per = ch[1] + ch[0] 
+        return [''.join(ch)] + [per]
+    if len(ch) < 2:
+        return ch
+    else:
+        return [ init+per for init in ch for per in permute(''.join(ch).replace(init,""))] 
+if __name__ == '__main__':
+    func()
